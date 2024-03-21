94cb032664b8aade9e2e7e624a502f689b819cc0
Alexey Barashkov <barasher@yandex.ru>
2024-03-22 00:16:35 +0500

    Write all the code

--- main.cpp
+++ main.cpp
@@ -1,13 +1,19 @@
 #include <algorithm>
 #include <cstdint>
+#include <cstdlib>
 #include <iostream>
 #include <limits>
 #include <map>
 #include <vector>
+#include <stdexcept>
 #include <unordered_map>
 
 static_assert (std::numeric_limits<std::uint32_t>::max() > 1000000U,
                "std::uint32_t size is not sufficient");
+static_assert (std::numeric_limits<std::int32_t>::max() > 1000000U,
+               "std::int32_t size is not sufficient");
+static_assert (std::numeric_limits<std::int32_t>::max() < std::numeric_limits<std::size_t>::max(),
+               "max value of std::int32_t should be lower than max_value of std::size_t");
 
 //TODO!!! add documentation, rewrite comments
 
@@ -53,7 +59,15 @@ bool RaySegmentsInLine::has_intersection(std::uint32_t orthogonal_line_position)
 
 //TODO!!!=========================================
 
-using RaySegmentsMap = std::unordered_map<std::uint32_t, RaySegmentsInLine>;
+using RaySegmentsMap = std::map<std::uint32_t, RaySegmentsInLine>;
+
+struct RaySegment final {
+  std::uint32_t first_coordinate{0U};
+  std::uint32_t second_coordinate_start{0U};
+  std::uint32_t second_coordinate_end{0U};
+};
+
+using RaySegments = std::vector<RaySegment>;
 
 struct Point final {
   std::uint32_t row{0U};
@@ -62,7 +76,7 @@ struct Point final {
 
 enum class SafeCheckResultType : std::int8_t {
   OpensWithoutInserting,
-  CannotBeOpened,
+  CanNotBeOpened,
   RequiresMirrorInsertion
 };
 
@@ -87,13 +101,13 @@ private:
                       bool start_direction_horizontal,
                       Point& end_point,
                       bool& end_direction_horizontal,
-                      RaySegmentsMap& row_wise_segments_map,
-                      RaySegmentsMap& col_wise_segments_map) const;
+                      RaySegments& row_wise_segments,
+                      RaySegments& col_wise_segments) const;
 
-  std::vector<Point> find_intersections(const RaySegmentsMap& forward_row_wise_segments_map,
-                                        const RaySegmentsMap& forward_col_wise_segments_map,
-                                        const RaySegmentsMap& backward_row_wise_segments_map,
-                                        const RaySegmentsMap& backward_col_wise_segments_map) const;
+  std::vector<Point> find_intersections_(const RaySegments& forward_row_wise_segments,
+                                         const RaySegments& forward_col_wise_segments,
+                                         const RaySegments& backward_row_wise_segments,
+                                         const RaySegments& backward_col_wise_segments) const;
 
   std::uint32_t rows_;
   std::uint32_t cols_;
@@ -102,6 +116,20 @@ private:
 };
 
 //TODO!!!=========================================
+
+namespace {
+
+RaySegmentsMap ray_segments_to_map(const RaySegments& ray_segments)
+{
+  RaySegmentsMap result;
+  for (const auto& segment : ray_segments) {
+    result[segment.first_coordinate]
+        .add_segment(segment.second_coordinate_start, segment.second_coordinate_end);
+  }
+  return result;
+}
+
+}
 
 SafeChecker::SafeChecker(std::uint32_t rows, std::uint32_t columns,
                          const std::vector<Point>& left_to_up_mirrors,
@@ -134,15 +162,15 @@ SafeCheckResult SafeChecker::check_safe() const
   bool forward_direction_is_positive = true;  // Left to right or up to down
   Point forward_end_point;
   bool forward_end_direction_horizontal = false;
-  RaySegmentsMap forward_row_wise_segments_map;
-  RaySegmentsMap forward_col_wise_segments_map;
+  RaySegments forward_row_wise_segments;
+  RaySegments forward_col_wise_segments;
   trace_the_ray_(forward_start_position,
                  forward_direction_is_horizontal,
                  forward_direction_is_positive,
                  forward_end_point,
                  forward_end_direction_horizontal,
-                 forward_row_wise_segments_map,
-                 forward_col_wise_segments_map);
+                 forward_row_wise_segments,
+                 forward_col_wise_segments);
 
   // Check if the safe can be opened without any mirror insertion
   if (forward_end_direction_horizontal &&
@@ -158,18 +186,62 @@ SafeCheckResult SafeChecker::check_safe() const
   bool backward_direction_is_positive = false;  // Left to right or up to down
   Point backward_end_point;
   bool backward_end_direction_horizontal = false;
-  RaySegmentsMap backward_row_wise_segments_map;
-  RaySegmentsMap backward_col_wise_segments_map;
+  RaySegments backward_row_wise_segments;
+  RaySegments backward_col_wise_segments;
   trace_the_ray_(backward_start_position,
                  backward_direction_is_horizontal,
                  backward_direction_is_positive,
                  backward_end_point,
                  backward_end_direction_horizontal,
-                 backward_row_wise_segments_map,
-                 backward_col_wise_segments_map);
+                 backward_row_wise_segments,
+                 backward_col_wise_segments);
 
   // Find intersections
-  // TODO!!!
+  const std::vector<Point> intersections = find_intersections_(forward_row_wise_segments,
+                                                               forward_col_wise_segments,
+                                                               backward_row_wise_segments,
+                                                               backward_col_wise_segments);
+
+  // Collect all intersections, which don't already contain a mirror
+  std::vector<Point> valid_intersections;
+  for (const auto& intersection: intersections) {
+    const auto mirror_row_iter = row_wise_mirrors_.find(intersection.row);
+    if (mirror_row_iter == row_wise_mirrors_.end()) {
+      valid_intersections.push_back(intersection);
+    } else {
+      const auto& mirror_row = mirror_row_iter->second;
+      const auto mirror_col_iter = mirror_row.find(intersection.col);
+      if (mirror_col_iter == mirror_row.end()) {
+        valid_intersections.push_back(intersection);
+      }
+    }
+  }
+
+  // Can not be opened if no intersections
+  if (valid_intersections.empty()) {
+    result.result_type = SafeCheckResultType::CanNotBeOpened;
+    return result;
+  }
+
+  // Find the lexicographically smallest mirror position
+  result.result_type = SafeCheckResultType::RequiresMirrorInsertion;
+  if (valid_intersections.size() < std::numeric_limits<std::uint32_t>::max()) {
+    result.positions = static_cast<std::uint32_t>(valid_intersections.size());
+  } else {
+    throw std::logic_error{"Internal logic error: intersections count is greater than maximum uint32"};
+  }
+  auto points_comparer = [] (const Point& first, const Point& second) {
+    return first.row != second.row ? first.row < second.row : first.col < second.col;
+  };
+  const auto min_element_iter =
+      std::min_element(valid_intersections.begin(), valid_intersections.end(), points_comparer);
+
+  if (min_element_iter == valid_intersections.end()) {
+    throw std::logic_error{"Failed to found min possible intersection"};
+  }
+  result.mirror_row = min_element_iter->row;
+  result.mirror_col = min_element_iter->col;
+
   return result;
 }
 
@@ -179,11 +251,11 @@ void SafeChecker::trace_the_ray_(const Point& start_point,
                                  bool start_direction_horizontal,
                                  Point& end_point,
                                  bool& end_direction_horizontal,
-                                 RaySegmentsMap& row_wise_segments_map,
-                                 RaySegmentsMap& col_wise_segments_map) const
+                                 RaySegments& row_wise_segments,
+                                 RaySegments& col_wise_segments) const
 {
-  row_wise_segments_map.clear();
-  col_wise_segments_map.clear();
+  row_wise_segments.clear();
+  col_wise_segments.clear();
 
   bool direction_is_positive = start_direction_positive;
   bool direction_is_horizontal = start_direction_horizontal;
@@ -225,7 +297,9 @@ void SafeChecker::trace_the_ray_(const Point& start_point,
           }
         }
         // Add a segment
-        row_wise_segments_map[current_position.row].add_segment(current_position.col, next_position.col);
+        const auto min_max_cols_pair = std::minmax(current_position.col, next_position.col);
+        RaySegment segment{current_position.row, min_max_cols_pair.first, min_max_cols_pair.second};
+        row_wise_segments.push_back(segment);
         // Go to the next position
         current_position = next_position;
       }
@@ -264,7 +338,9 @@ void SafeChecker::trace_the_ray_(const Point& start_point,
           }
         }
         // Add a segment
-        col_wise_segments_map[current_position.col].add_segment(current_position.row, next_position.row);
+        const auto min_max_rows_pair = std::minmax(current_position.row, next_position.row);
+        RaySegment segment{current_position.col, min_max_rows_pair.first, min_max_rows_pair.second};
+        row_wise_segments.push_back(segment);
         // Go to the next position
         current_position = next_position;
       }
@@ -274,19 +350,110 @@ void SafeChecker::trace_the_ray_(const Point& start_point,
   end_direction_horizontal = direction_is_horizontal;
 }
 
-std::vector<Point> SafeChecker::find_intersections(const RaySegmentsMap& forward_row_wise_segments_map,
-                                                   const RaySegmentsMap& forward_col_wise_segments_map,
-                                                   const RaySegmentsMap& backward_row_wise_segments_map,
-                                                   const RaySegmentsMap& backward_col_wise_segments_map) const
+std::vector<Point> SafeChecker::find_intersections_(const RaySegments& forward_row_wise_segments,
+                                                    const RaySegments& forward_col_wise_segments,
+                                                    const RaySegments& backward_row_wise_segments,
+                                                    const RaySegments& backward_col_wise_segments) const
 {
+  std::vector<Point> intersections;
+  const RaySegmentsMap forward_row_wise_segments_map = ray_segments_to_map(forward_row_wise_segments);
+  const RaySegmentsMap forward_col_wise_segments_map = ray_segments_to_map(forward_col_wise_segments);
 
+  for (const auto& segment : backward_row_wise_segments) {
+    const std::uint32_t row = segment.first_coordinate;
+    const auto col_iter = forward_col_wise_segments_map.lower_bound(segment.second_coordinate_start);
+    while (col_iter != forward_col_wise_segments_map.end() && col_iter->first <= segment.second_coordinate_end) {
+      const std::uint32_t col = col_iter->first;
+      if (col_iter->second.has_intersection(row)) {
+        Point intersection{row, col};
+        intersections.push_back(intersection);
+      }
+    }
+  }
+  for (const auto& segment : backward_col_wise_segments) {
+    const std::uint32_t col = segment.first_coordinate;
+    const auto row_iter = forward_row_wise_segments_map.lower_bound(segment.second_coordinate_start);
+    while (row_iter != forward_row_wise_segments_map.end() && row_iter->first <= segment.second_coordinate_end) {
+      const std::uint32_t row = row_iter->first;
+      if (row_iter->second.has_intersection(col)) {
+        Point intersection{row, col};
+        intersections.push_back(intersection);
+      }
+    }
+  }
+  return intersections;
 }
 
 }  // namespace mirrors_lasers
 
+namespace {
+
+constexpr std::int32_t MAX_SIDE{1000000};
+constexpr std::int32_t MAX_MIRRORS{200000};
+
+std::vector<mirrors_lasers::Point> input_mirrors(std::int32_t count, std::int32_t r, std::int32_t c)
+{
+  std::vector<mirrors_lasers::Point> mirrors;
+  mirrors.reserve(static_cast<std::size_t>(count));
+  std::int32_t ri{};
+  std::int32_t ci{};
+  for (std::int32_t i = 0; i < count; ++i) {
+    std::cin >> ri >> ci;
+    if (ri < 1 || ri > r) {
+      throw std::invalid_argument("Incorrect ri value");
+    }
+    if (ci < 1 || ci > c) {
+      throw std::invalid_argument("Incorrect ci value");
+    }
+    mirrors_lasers::Point mirror{static_cast<std::uint32_t>(ri), static_cast<std::uint32_t>(ci)};
+    mirrors.push_back(mirror);
+  }
+  return mirrors;
+}
+
+}  // namespace
+
 int main()
 {
-  // Check input range
-  std::cout << "Hello World!" << std::endl;
-  return 0;
+  std::int32_t r{};
+  std::int32_t c{};
+  std::int32_t m{};
+  std::int32_t n{};
+  std::cin >> r >> c >> m >> n;
+  if (r < 1 || r > MAX_SIDE) {
+    std::cerr << "Incorrect r value" << std::endl;
+    return EXIT_FAILURE;
+  }
+  if (c < 1 || c > MAX_SIDE) {
+    std::cerr << "Incorrect c value" << std::endl;
+    return EXIT_FAILURE;
+  }
+  if (m < 0 || m > MAX_MIRRORS) {
+    std::cerr << "Incorrect m value" << std::endl;
+    return EXIT_FAILURE;
+  }
+  if (n < 0 || n > MAX_MIRRORS) {
+    std::cerr << "Incorrect n value" << std::endl;
+    return EXIT_FAILURE;
+  }
+
+  std::vector<mirrors_lasers::Point> left_to_up_mirrors = input_mirrors(m, r, c);
+  std::vector<mirrors_lasers::Point> left_to_down_mirrors = input_mirrors(n, r, c);
+
+  const mirrors_lasers::SafeChecker checker{static_cast<std::uint32_t>(r),
+                                            static_cast<std::uint32_t>(c),
+                                            left_to_up_mirrors,
+                                            left_to_down_mirrors};
+
+  const mirrors_lasers::SafeCheckResult check_result = checker.check_safe();
+
+  if (check_result.result_type == mirrors_lasers::SafeCheckResultType::OpensWithoutInserting) {
+    std::cout << 0 << std::endl;
+  } else if (check_result.result_type == mirrors_lasers::SafeCheckResultType::CanNotBeOpened) {
+    std::cout << -1 << std::endl;
+  } else if (check_result.result_type == mirrors_lasers::SafeCheckResultType::RequiresMirrorInsertion) {
+    std::cout << check_result.positions << " " << check_result.mirror_row << " " << check_result.mirror_col << std::endl;
+  }
+
+  return EXIT_SUCCESS;
 }
